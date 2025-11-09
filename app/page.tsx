0dd9f22acdebe81ed2c0524e0a4ca5279fd4969d
juyoung <vilioite@naver.com>
2025-11-09 14:10:57 +0900

    refactor, fix: 중복 코드 제거, 꼬인 높이 계산 정리

--- app/page.tsx
+++ app/page.tsx
@@ -193,7 +193,7 @@ export default function MainPage() {
 
         ScrollTrigger.create({
           trigger: projectRef.current,
-          start: `top+=${i * 1000} center`,
+          start: `top+=${i * 1000} bottom+=700`,
           onEnter: () => gsap.to(project, { scale: 1, duration: 0.2 }),
           onLeaveBack: () => gsap.to(project, { scale: 0.8, duration: 0.2 }),
           scrub: true,
@@ -249,7 +249,7 @@ export default function MainPage() {
                   YOUNG'S PORTFOLIO
                 </h1>
                 <button
-                  className="px-8 py-2.5 bg-white rounded-full text-black body3 font-semibold flex items-center justify-center gap-3 xl:gap-4 hover:s`ca`le-110"
+                  className="px-8 py-2.5 bg-white rounded-full text-black body3 font-semibold flex items-center justify-center gap-3 xl:gap-4 hover:scale-110"
                   onClick={() => {
                     window.location.href = "/about";
                   }}
@@ -266,152 +266,154 @@ export default function MainPage() {
                 </button>
               </div>
             </div>
+
             <div
               className="absolute top-0 left-0 w-full h-full"
               ref={contentRef}
             >
-              <div className="bg-black w-full h-[500vh] xl:h-[460vh]">
-                <div className="w-full h-[200vh]">
-                  <div className="w-full h-screen flex flex-col items-center justify-center sticky top-0">
-                    <p
-                      className="absolute px-12 top-20 left-0 body1 font-semibold text-outline-white
+              <div className="bg-black w-full h-[200vh]">
+                <div className="w-full h-screen flex flex-col items-center justify-center sticky top-0">
+                  <p
+                    className="absolute px-12 top-20 left-0 body1 font-semibold text-outline-white
                       text-justify xl:top-16"
-                    >
-                      BLENDING TECHNOLOGY AND EMOTION, CREATING EXPERIENCES THAT
-                      GO BEYOND WORDS AND RESONATE WITH THE HEART.
-                    </p>
-                    <p
-                      className="body2 text-white
+                  >
+                    BLENDING TECHNOLOGY AND EMOTION, CREATING EXPERIENCES THAT
+                    GO BEYOND WORDS AND RESONATE WITH THE HEART.
+                  </p>
+                  <p
+                    className="body2 text-white
                       absolute bottom-56 text-center xl:bottom-80"
-                    >
-                      현실과 감성의 경계를 넘나들며 마음에 닿는 경험과 공간을
-                      디자인합니다.
-                      <br />
-                      감성과 기술로 연결된 공간을 그리는 디자이너, 최주영입니다.
-                    </p>
-                    <div className="w-[40vw] h-[40vh]">
-                      <Scene />
-                    </div>
+                  >
+                    현실과 감성의 경계를 넘나들며 마음에 닿는 경험과 공간을
+                    디자인합니다.
+                    <br />
+                    감성과 기술로 연결된 공간을 그리는 디자이너, 최주영입니다.
+                  </p>
+                  <div className="w-[40vw] h-[40vh]">
+                    <Scene />
+                  </div>
+                  <div
+                    className="absolute bottom-28 xl:bottom-40"
+                    ref={scrollRef}
+                  >
+                    <Arrow
+                      width={iconScrollArrowSize}
+                      height={iconScrollArrowSize}
+                      fill={"#fff"}
+                      className="animate-bounce"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="w-full bg-black relative h-[800vh] xl:h-[750vh]"
+                ref={photoRef}
+              >
+                <div className="w-full h-screen sticky top-0 flex items-center justify-center overflow-x-hidden">
+                  <div className="h-full relative flex flex-col items-center justify-center zoom-photo">
+                    <Image
+                      src="/images/img-main-background.jpg"
+                      alt="배경 이미지"
+                      fill
+                      className="object-cover brightness-75"
+                    />
                     <div
-                      className="absolute bottom-28 xl:bottom-40"
-                      ref={scrollRef}
-                    >
-                      <Arrow
-                        width={iconScrollArrowSize}
-                        height={iconScrollArrowSize}
-                        fill={"#fff"}
-                        className="animate-bounce"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full h-[160%]" ref={photoRef}>
-                  <div className="w-full h-screen sticky top-0 flex items-center justify-center overflow-x-hidden">
-                    <div className="h-full relative flex flex-col items-center justify-center zoom-photo">
-                      <Image
-                        src="/images/img-main-background.jpg"
-                        alt="배경 이미지"
-                        fill
-                        className="object-cover brightness-75"
-                      />
-                      <div
-                        className="absolute top-0 left-0 bg-black w-full h-screen"
-                        ref={photoBackgroundRef}
-                      />
-                      <h2 className="text-gray-10 text-center font-semibold relative z-10 flex flex-col items-center justify-center photo-words h1">
-                        <span className="transition-all duration-700">
-                          IMAGINE BEYOND
-                        </span>
-                        <div className="flex flex-col items-center transition-all duration-700">
-                          <div
-                            className="flex relative bottom-[34px] my-8 xl:my-12 xl:bottom-[50px]"
-                            style={{
-                              clipPath:
-                                "polygon(0 0, 100% 0, 100% 200%, 0 200%)",
-                            }}
-                          >
-                            <span className="bouncing-loop h-10">x</span>
-                          </div>
-                          <span>SOME PROJECTS</span>
+                      className="absolute top-0 left-0 bg-black w-full h-screen"
+                      ref={photoBackgroundRef}
+                    />
+                    <h2 className="text-gray-10 text-center font-semibold relative z-10 flex flex-col items-center justify-center photo-words h1">
+                      <span className="transition-all duration-700">
+                        IMAGINE BEYOND
+                      </span>
+                      <div className="flex flex-col items-center transition-all duration-700">
+                        <div
+                          className="flex relative bottom-[34px] my-8 xl:my-12 xl:bottom-[50px]"
+                          style={{
+                            clipPath: "polygon(0 0, 100% 0, 100% 200%, 0 200%)",
+                          }}
+                        >
+                          <span className="bouncing-loop h-10">x</span>
                         </div>
-                      </h2>
+                        <span>SOME PROJECTS</span>
+                      </div>
+                    </h2>
+                  </div>
+                </div>
+
+                <div
+                  className="bg-black w-full absolute top-[400vh] xl:top-[350vh]"
+                  ref={projectRef}
+                >
+                  <div className="absolute top-0 left-0 w-[40vw] h-[60vh] bg-black text-white project-item">
+                    <div className="w-full h-full">
+                      <Image
+                        src="/images/img-project-thumbnail-youngfolio.jpg"
+                        alt="YOUNGFOLIO 썸네일"
+                        fill
+                        className="object-cover border border-gray-40"
+                      />
                     </div>
+                    <div className="relative mt-8 flex flex-col gap-2 ml-4">
+                      <h3 className="h5">YOUNGFOLIO</h3>
+                      <p className="body5">
+                        2025. WEB / UX JUYOUNG'S PORTFOLIO
+                      </p>
+                    </div>
+                  </div>
+                  <div className="absolute top-[100vh] right-0 w-[40vw] h-[60vh] bg-black text-white project-item">
+                    <div className="w-full h-full">
+                      <Image
+                        src="/images/img-project-thumbnail-youngfolio.jpg"
+                        alt="YOUNGFOLIO 썸네일"
+                        fill
+                        className="object-cover border border-gray-40"
+                      />
+                    </div>
+                    <div className="relative mt-8 flex flex-col gap-2 ml-4">
+                      <h3 className="h5">YOUNGFOLIO</h3>
+                      <p className="body5">
+                        2025. WEB / UX JUYOUNG'S PORTFOLIO
+                      </p>
+                    </div>
+                  </div>
+                  <div className="absolute top-[200vh] left-0 w-[40vw] h-[60vh] bg-black text-white project-item">
+                    <div className="w-full h-full">
+                      <Image
+                        src="/images/img-project-thumbnail-youngfolio.jpg"
+                        alt="YOUNGFOLIO 썸네일"
+                        fill
+                        className="object-cover border border-gray-40"
+                      />
+                    </div>
+                    <div className="relative mt-8 flex flex-col gap-2 ml-4">
+                      <h3 className="h5">YOUNGFOLIO</h3>
+                      <p className="body5">
+                        2025. WEB / UX JUYOUNG'S PORTFOLIO
+                      </p>
+                    </div>
+                  </div>
+                  <div className="absolute top-[300vh] left-0 right-0 mx-auto w-[40vw] h-[60vh] bg-black text-white project-item">
+                    <div className="w-full h-full">
+                      <Image
+                        src="/images/img-project-thumbnail-youngfolio.jpg"
+                        alt="YOUNGFOLIO 썸네일"
+                        fill
+                        className="object-cover border border-gray-40"
+                      />
+                    </div>
+                    <div className="relative pt-8 flex flex-col gap-2 ml-4 bg-black">
+                      <h3 className="h5">YOUNGFOLIO</h3>
+                      <p className="body5">
+                        2025. WEB / UX JUYOUNG'S PORTFOLIO
+                      </p>
+                    </div>
+                    <div className="w-full h-[20vh] bg-black" />
                   </div>
                 </div>
               </div>
-              <div className="bg-black pt-[1000px] w-full" ref={projectRef}>
-                <div className="w-full sticky">
-                  <div className="relative w-full">
-                    <div className="absolute top-0 left-0 w-[40vw] h-[60vh] bg-black text-white project-item">
-                      <div className="w-full h-full">
-                        <Image
-                          src="/images/img-project-thumbnail-youngfolio.jpg"
-                          alt="YOUNGFOLIO 썸네일"
-                          fill
-                          className="object-cover border border-gray-40"
-                        />
-                      </div>
-                      <div className="relative mt-8 flex flex-col gap-2 ml-4">
-                        <h3 className="h5">YOUNGFOLIO</h3>
-                        <p className="body5">
-                          2025. WEB / UX JUYOUNG'S PORTFOLIO
-                        </p>
-                      </div>
-                    </div>
-                    <div className="absolute top-[100vh] right-0 w-[40vw] h-[60vh] bg-black text-white project-item">
-                      <div className="w-full h-full">
-                        <Image
-                          src="/images/img-project-thumbnail-youngfolio.jpg"
-                          alt="YOUNGFOLIO 썸네일"
-                          fill
-                          className="object-cover border border-gray-40"
-                        />
-                      </div>
-                      <div className="relative mt-8 flex flex-col gap-2 ml-4">
-                        <h3 className="h5">YOUNGFOLIO</h3>
-                        <p className="body5">
-                          2025. WEB / UX JUYOUNG'S PORTFOLIO
-                        </p>
-                      </div>
-                    </div>
-                    <div className="absolute top-[200vh] left-0 w-[40vw] h-[60vh] bg-black text-white project-item">
-                      <div className="w-full h-full">
-                        <Image
-                          src="/images/img-project-thumbnail-youngfolio.jpg"
-                          alt="YOUNGFOLIO 썸네일"
-                          fill
-                          className="object-cover border border-gray-40"
-                        />
-                      </div>
-                      <div className="relative mt-8 flex flex-col gap-2 ml-4">
-                        <h3 className="h5">YOUNGFOLIO</h3>
-                        <p className="body5">
-                          2025. WEB / UX JUYOUNG'S PORTFOLIO
-                        </p>
-                      </div>
-                    </div>
-                    <div className="absolute top-[300vh] left-0 right-0 mx-auto w-[40vw] h-[60vh] bg-black text-white project-item">
-                      <div className="w-full h-full">
-                        <Image
-                          src="/images/img-project-thumbnail-youngfolio.jpg"
-                          alt="YOUNGFOLIO 썸네일"
-                          fill
-                          className="object-cover border border-gray-40"
-                        />
-                      </div>
-                      <div className="relative pt-8 flex flex-col gap-2 ml-4 bg-black">
-                        <h3 className="h5">YOUNGFOLIO</h3>
-                        <p className="body5">
-                          2025. WEB / UX JUYOUNG'S PORTFOLIO
-                        </p>
-                      </div>
-                      <div className="w-full h-[20vh] bg-black" />
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full h-[360vh] bg-black" />
-                <div className="w-full h-[400vh] bg-white" />
-              </div>
+
+              <div className="w-full h-[400vh] bg-white"></div>
             </div>
           </>
         )}
